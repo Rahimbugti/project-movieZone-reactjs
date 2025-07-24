babca8689ff794f0cad8075554c158f33bd11f2a
Rahimbugti <162613347+Rahimbugti@users.noreply.github.com>
2025-07-25 00:32:07 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Login from './components/Login'
+import './App.css'
+import'./index.css'
+ import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import Movies from './MovieZone/Movies'
+
+const App = () => {
+  return (
+    <div>
+        <Movies/>
+
+    </div>
+  )
+}
+
+export default App
